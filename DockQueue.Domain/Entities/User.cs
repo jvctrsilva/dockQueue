a230b31dbdec53bfeb98eeed002a7256a1dc3eca
João Victor <joaovictorsilva0902@gmail.com>
2025-09-01 22:49:27 -0300

    Add new projects and update application structure
    - Updated solution file to include `DockQueue.Infra.Data` and `DockQueue.Infra.Ioc`.
    - Created `DockQueue.Application` and `DockQueue.Domain` projects with `net8.0` target framework.
    - Added `User` and `DomainExceptionValidation` classes for user management and validation.
    - Implemented `ApplicationDbContext` and `UserConfiguration` for database interactions.
    - Configured logging with `appsettings` files and set up Swagger in `Program.cs`.
    - Created `WeatherForecast` and `WeatherForecastController` for weather API functionality.
    - Added launch settings for development environment.

---
+++ DockQueue.Domain/Entities/User.cs
@@ -0,0 +1,43 @@
+﻿using DockQueue.Domain.Validation;
+
+namespace DockQueue.Domain.Entities;
+
+public class User
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; } 
+    public string Number { get; private set; } 
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public string Role { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public User(string name, string number, string email,
+                string password, string role, DateTime createdAt)
+    {
+        ValidateDomain(name, number, email, password, role, createdAt); 
+    }
+    public User(int id, string name, string number, string email,
+            string password, string role, DateTime createdAt)
+    {
+        Id = id;
+        ValidateDomain(name, number, email, password, role, createdAt);
+    }
+    public void Update(string name, string number, string email,
+                       string password, string role, DateTime createdAt)
+    {   
+        ValidateDomain(name, number, email, password, role, createdAt);
+    }
+    public void ValidateDomain(string name, string number, string email,
+                               string password, string role, DateTime createdAt)
+    {
+        DomainExceptionValidation.When(name.Length > 200, "Nome inválido, muito longo, máximo 200 caracteres");
+
+        Name = name;
+        Number = number;
+        Email = email;
+        Password = password;
+        Role = role;
+        CreatedAt = createdAt;
+    }
+}
